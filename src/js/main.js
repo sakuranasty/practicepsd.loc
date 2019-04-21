fc7fd98e8a0d1051853ac3eabae68e1bc7888359
ÐAPavlova <sakuranasty@mail.ru>
2019-04-22 00:42:11 +0300

    update

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+  $('.sliderMain').slick({
+    accessibility: true,
+    adaptiveHeight: true,
+    autoplay:true,
+    draggable:true,
+    infinite:true,
+    arrows:true,
+    dots:true,
+    slidesToShow:1,
+    responsive:true
+  });
+});
