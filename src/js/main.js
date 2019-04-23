afa7494c027d39981227d5bdbcc2e9129c8eb9b6
ÐAPavlova <sakuranasty@mail.ru>
2019-04-24 01:21:37 +0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,32 @@
-
+function cb(){return}
 $(document).ready(function(){
   $('.sliderMain').slick({
     accessibility: true,
-    adaptiveHeight: true,
-    autoplay:false,
+    adaptiveHeight: false,
+    autoplay:true,
     draggable:true,
     infinite:true,
-    arrows:true,
+    arrows:false,
     dots:true,
     slidesToShow:1,
     responsive:true
   });
+  $('#hMenu').click(function(){
+  	$('.header-nav').slideToggle(400)
+  	});
+  $('#hMenu').focusout(function(){
+  	$('.header-nav').slideToggle(400)
+  	});
+  $('.header-nav a').click(function(){
+  	$('#hMenu').triggerHandler('focusout')
+  })
+  $('#tbMenu').click(function(){
+  	$('.top-bar ul').slideToggle(400)
+  	});
+  $('#tbMenu').focusout(function(){
+  	$('.top-bar ul').slideToggle(400)
+  	});
+  $('#scroll').click(function(){
+  	$(window).scrollTop(0)
+  	});
 });
